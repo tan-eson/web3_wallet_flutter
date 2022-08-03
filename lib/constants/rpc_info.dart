1f21b39e0cf709a27b20d2408ca887933c374008
Tan Eson <tanesonjpg@gmail.com>
2022-08-03 22:02:39 +0800

    - [Fix] flutter secure storage upgrade version for web compatibility

--- lib/constants/rpc_info.dart
+++ lib/constants/rpc_info.dart
@@ -1,7 +1,8 @@
 import 'package:web3_wallet_flutter/constants/eth.dart';
 import 'package:web3_wallet_flutter/constants/eth_goerli.dart';
+import 'package:web3_wallet_flutter/constants/network_template.dart';
 
 class RPCInfo {
-  static EthereumMainnet ethereumMainnet = EthereumMainnet();
-  static GoerliTestnet goerliTestnet = GoerliTestnet();
+  static Network ethereumMainnet = EthereumMainnet();
+  static Network goerliTestnet = GoerliTestnet();
 }
